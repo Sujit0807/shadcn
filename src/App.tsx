1c0e29995b064a533e3191655bfee3f62fa976bd
Sujit Mali <malisujit12@gmail.com>
2024-06-12 00:07:58 +0530

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,31 @@
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+
+function App() {
+  return (
+    <Dialog>
+      <div className="flex justify-center">
+        <DialogTrigger className="font-red-500">
+          Hello Open This Dialog
+        </DialogTrigger>
+      </div>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Are you absolutely sure?</DialogTitle>
+          <DialogDescription>
+            This action cannot be undone. This will permanently delete your
+            account and remove your data from our servers.
+          </DialogDescription>
+        </DialogHeader>
+      </DialogContent>
+    </Dialog>
+  );
+}
+
+export default App;
